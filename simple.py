67b6157c48e8de6f045e20457a2f98dbf20c6acb
Chu Fang <chufang@usc.edu>
2022-10-10 22:12:31 -0700

    added roberta-large/distilbert-word2vec

--- simple.py
+++ simple.py
@@ -1,3 +1,7 @@
+import os
+
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+import torch
 from flask import Flask, request, render_template
 from transformers import BertModel, BertConfig, BertTokenizer, FlaxBertForQuestionAnswering, pipeline
 
@@ -22,46 +26,72 @@ def search():
     result = {}
     sentence = "{A} is to {B} as {C} is to {D}."
     return render_template('fourBlanks.html',
-                           result=fillMaskWithModels(sentence, ['bert-base-uncased', 'roberta-base']))
+                           result=fillMaskWithModels(sentence, ['bert-base-uncased', 'roberta-base', 'roberta-large', 'vocab-transformers/distilbert-word2vec_256k-MLM_1M']))
+
+
+def findIndex(result, target):
+    for index, item in enumerate(result):
+        if item['token_str'] == target.lower():
+            return index
+    return None
 
 
 def fillMaskWithModels(sentence, models):
-    result = []
+    result = {'B': [], 'D': []}  # {B: [{}, {}, {}], D:[{}, {}, {}]}
     for model in models:
         mask = 'NoneNone'
-        if model == 'bert-base-uncased':
+        if model == 'bert-base-uncased' or model == 'vocab-transformers/distilbert-word2vec_256k-MLM_1M':
             mask = '[MASK]'
-        elif model == 'roberta-base':
+        elif model == 'roberta-base' or model == 'roberta-large':
             mask = '<mask>'
         tmp1 = sentence.format(A=request.form['A'], B=request.form['B'], C=request.form['C'], D=mask)
         tmp2 = sentence.format(A=request.form['A'], B=mask, C=request.form['C'], D=request.form['D'])
-        tmpList = []
-        unmasker = pipeline('fill-mask', model=model, top_k=20)
+        unmasker = pipeline('fill-mask', model=model, top_k=2000, device=torch.device("mps"))
         if mask in tmp1:
             tmp1result = unmasker(tmp1)
             topD, topDScore = tmp1result[0]['token_str'], tmp1result[0]['score']
             trueD, trueDScore = request.form['D'], unmasker(tmp1, targets=request.form['D'])[0]['score']
-            idx = next((index for (index, d) in enumerate(tmp1result) if d["token_str"] == request.form['D']), None)
-            reciprocalRank = 1.0 / (idx + 1) if idx != None else 0
-            tmpList.append(
-                model + "'s top prediction: " + tmp1.replace(mask, "*" + topD) + " " + "Score: " + str(topDScore))
-            tmpList.append(
-                model + "'s score for your answer: " + tmp1.replace(mask, "*" + trueD) + " " + "Score: " + str(
-                    trueDScore) + " " + "Reciprocal Rank: " + str(reciprocalRank))
+            idx = findIndex(tmp1result, request.form['D'])
+            reciprocalRank = 1.0 / (idx + 1) if idx is not None else 'NA'
+            result['D'].append({'model': model, 'topD': topD, 'topDScore': topDScore, 'topDRank': 1.0,
+                                 'topDSentence': tmp1.replace(mask, "*" + topD), 'trueD': trueD,
+                                 'trueDScore': trueDScore,
+                                 'trueDRank': reciprocalRank, 'trueDSentence': tmp1.replace(mask, "*" + trueD)})
+            # tmpList.append(
+            # model + "'s top prediction: " + tmp1.replace(mask, "*" + topD) + " " + "Score: " + str(topDScore))
+            # tmpList.append(
+            #     model + "'s score for your answer: " + tmp1.replace(mask, "*" + trueD) + " " + "Score: " + str(
+            #         trueDScore) + " " + "Reciprocal Rank: " + str(reciprocalRank))
         if mask in tmp2:
             tmp2result = unmasker(tmp2)
             topB, topBScore = tmp2result[0]['token_str'], tmp2result[0]['score'],
             trueB, trueBScore = request.form['B'], unmasker(tmp2, targets=request.form['B'])[0]['score']
-            idx = next((index for (index, d) in enumerate(tmp2result) if d["token_str"] == request.form['B']), None)
-            reciprocalRank = 1.0 / (idx + 1) if idx != None else 0
-            tmpList.append(
-                model + "'s top prediction: " + tmp1.replace(mask, "*" + topB) + " " + "Score: " + str(topBScore))
-            tmpList.append(
-                model + "'s score for your answer: " + tmp1.replace(mask, "*" + trueB) + " " + "Score: " + str(
-                    trueBScore) + " " + "Reciprocal Rank: " + str(reciprocalRank))
-        result.append(tmpList)
+            idx = findIndex(tmp2result, request.form['B'])
+            reciprocalRank = 1.0 / (idx + 1) if idx is not None else 'NA'
+            result['B'].append({'model': model, 'topD': topB, 'topDScore': topBScore, 'topDRank': 1.0,
+                                 'topDSentence': tmp2.replace(mask, "*" + topB), 'trueD': trueB,
+                                 'trueDScore': trueBScore,
+                                 'trueDRank': reciprocalRank, 'trueDSentence': tmp2.replace(mask, "*" + trueB)})
+            # tmpList.append(
+            #     model + "'s top prediction: " + tmp2.replace(mask, "*" + topB) + " " + "Score: " + str(topBScore))
+            # tmpList.append(
+            #     model + "'s score for your answer: " + tmp2.replace(mask, "*" + trueB) + " " + "Score: " + str(
+            #         trueBScore) + " " + "Reciprocal Rank: " + str(reciprocalRank))
     return result
 
 
+def printConfig():
+    print(f"PyTorch version: {torch.__version__}")
+
+    # Check PyTorch has access to MPS (Metal Performance Shader, Apple's GPU architecture)
+    print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
+    print(f"Is MPS available? {torch.backends.mps.is_available()}")
+
+    # Set the device
+    device = "mps" if torch.backends.mps.is_available() else "cpu"
+    print(f"Using device: {device}")
+
+
 if __name__ == '__main__':
+    printConfig()
     app.run(port=8888)
